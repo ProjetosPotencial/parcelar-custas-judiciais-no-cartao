7dc27a661499da2db45ffa2b5209dfb3a7af8617
ProjetosPotencial <projetos@potencialgrupo.com.br>
2026-06-15 13:30:20 -0300

    v6

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,30 +1,18 @@
-import fs from 'node:fs'
-import path from 'node:path'
 import { getGlobal } from './data'
 import { textOverrides, imageOverrides } from './overrides'
+import { TEMPLATE } from './template'
 
 export const dynamic = 'force-dynamic'
-
-const pub = (...p: string[]) => path.join(process.cwd(), 'public', ...p)
-const read = (f: string) => { try { return fs.readFileSync(f, 'utf8') } catch { return '' } }
 
 function esc(s: string): string {
   return String(s).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
 }
 function mediaUrl(v: any): string {
-  if (v && typeof v === 'object') return v.url || ''
-  return ''
+  return v && typeof v === 'object' ? v.url || '' : ''
 }
 
 export default async function Home() {
-  let html = read(pub('lp-template.html'))
-
-  // CSS embutido na mesma ordem do preview (lp-inline -> styles -> tailwind)
-  const css =
-    read(pub('css', 'lp-inline.css')) + '\n' +
-    read(pub('css', 'styles.css')) + '\n' +
-    read(pub('css', 'tailwind.css'))
-
+  let html = TEMPLATE
   const g = await getGlobal()
   for (const o of textOverrides) {
     const val = g?.[o.key]
@@ -34,11 +22,5 @@ export default async function Home() {
     const url = mediaUrl(g?.[o.key])
     if (url) html = html.split(o.find).join(url)
   }
-
-  return (
-    <>
-      <style dangerouslySetInnerHTML={{ __html: css }} />
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </>
-  )
+  return <div dangerouslySetInnerHTML={{ __html: html }} />
 }
